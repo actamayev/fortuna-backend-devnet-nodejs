6f255d2963b9c3800b70045c1c0547a8f1cf9bd5
atamayev <75034340+atamayev@users.noreply.github.com>
2024-03-16 22:08:09 -0400

    Initial Server setup (#1)
    * Initial Server setup
    
    * Update packages
    
    * Workflow fix

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+import { createServer } from "http"
+import cookieParser from "cookie-parser"
+
+import checkHealth from "./controllers/health-checks/check-health"
+
+dotenv.config()
+
+const port = parseInt(process.env.PORT, 10) || 8000
+
+// void connectDatabase()
+
+const app = express()
+
+const server = createServer(app)
+
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", req.headers.origin as string)
+	res.header("Access-Control-Allow-Credentials", "true")
+	next()
+})
+
+app.use(cors({
+	credentials: true,
+	origin: (origin, callback) => {
+		callback(null, true)
+	}
+}))
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.use("/health", checkHealth)
+
+app.use("*", (req, res) => res.status(404).json({ error: "Route not found"}))
+
+// Initialization of server:
+server.listen(port, "0.0.0.0", () => {
+	console.info(`Listening on port ${port}`)
+})
