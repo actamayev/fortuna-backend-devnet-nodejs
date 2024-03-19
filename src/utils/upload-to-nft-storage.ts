f7a1fd3bd728564e7d24ae92d19a7ec7c375324e
atamayev <75034340+atamayev@users.noreply.github.com>
2024-03-19 00:04:25 -0400

    Package updates. Still unable to upload with NFT Storage
    Likely to do with version mismatch.

--- src/utils/upload-to-nft-storage.ts
+++ src/utils/upload-to-nft-storage.ts
@@ -8,7 +8,7 @@ export default async function uploadToNFTStorage (
 	try {
 		const client = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY })
 
-		const blob = new Blob([fileBuffer], { type: "image/png" })
+		const blob = new Blob([fileBuffer], { type: "image/jpg" })
 
 		const metadata = await client.store({
 			name: "Tiger 1",
