fa657b61c41d3a811b4d7d2cf1cd95434874cd59
atamayev <75034340+atamayev@users.noreply.github.com>
2024-03-23 14:41:04 -0400

    Migration

--- sql/fiftyone_selects.sql
+++ sql/fiftyone_selects.sql
@@ -1,9 +1,16 @@
 -- select * from credentials;
 -- select * from login_history;
 -- select * from solana_wallet;
-select * from spl;
+-- select * from spl;
 -- select * from spl_mint;
 -- Select solana_wallet.user_id, public_key, secret_key, username, email from credentials join solana_wallet on credentials.user_id = solana_wallet.user_id;
 -- select * from spl_ownership;
 -- select * from token_account;
+-- select * from uploaded_image;
+
+-- To delete an spl:
+-- delete from spl_ownership;
+-- delete from spl_mint;
+-- delete from token_account;
+-- delete from spl;
 
