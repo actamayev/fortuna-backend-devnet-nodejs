1af7c54f669e41c7fe3ac2b245bed0fcd9bc4d49
atamayev <75034340+atamayev@users.noreply.github.com>
2024-03-24 19:09:24 -0400

    Cleanup

--- src/middleware/solana/confirm-not-sending-sol-to-self.ts
+++ src/middleware/solana/confirm-not-sending-sol-to-self.ts
@@ -6,7 +6,7 @@ export default function confirmNotSendingSolToSelf(req: Request, res: Response, 
 		const solanaWallet = req.solanaWallet
 		const recipientPublicKey = req.publicKey
 
-		if (_.isEqual( solanaWallet.public_key, recipientPublicKey.toString())) {
+		if (_.isEqual(solanaWallet.public_key, recipientPublicKey.toString())) {
 			return res.status(400).json({ message: "Cannot send sol to self" })
 		}
 
