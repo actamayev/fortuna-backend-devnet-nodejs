c7a6eadd5feb189ec37e353f4b1ac4d45a3b6963
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-04 21:17:17 -0400

    Get creator content list

--- src/controllers/solana/devnet/get-creator-content-list.ts
+++ src/controllers/solana/devnet/get-creator-content-list.ts
@@ -1,18 +1,34 @@
 import { Request, Response } from "express"
 import prismaClient from "../../../prisma-client"
+import transformCreatorContentList from "../../../utils/solana/transform-creator-content-list"
 
 export default async function getCreatorContentList(req: Request, res: Response): Promise<Response> {
 	try {
 		const solanaWallet = req.solanaWallet
 
-		const creatorSPLData = await prismaClient.spl.findMany(
-			{ where: {
+		const creatorSPLData = await prismaClient.spl.findMany({
+			where: {
 				creator_wallet_id: solanaWallet.solana_wallet_id
-			}}
-		)
+			},
+			select: {
+				spl_id: true,
+				spl_name: true,
+				total_number_of_shares: true,
+				listing_price_per_share_sol: true,
+				description: true,
+				initial_creator_ownership_percentage: true,
+				uploaded_image: {
+					select: {
+						image_url: true
+					}
+				},
+				public_key_address: true
+			}
+		})
 
-		// will need to perform joins on the spl data to retrieve all necessary data
-		return res.status(200).json({ })
+		const creatorContentList = transformCreatorContentList(creatorSPLData)
+
+		return res.status(200).json({ creatorContentList })
 	} catch (error) {
 		console.error(error)
 		return res.status(500).json({ error: "Internal Server Error: Unable to Retrieve Creator content list" })
