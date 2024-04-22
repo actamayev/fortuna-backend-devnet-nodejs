b587e13daee62e72fcba1a9fc22f9f2f1beb4cb2
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-22 10:11:10 -0400

    Add profile picture

---
+++ src/controllers/upload/upload-profile-picture.ts
@@ -0,0 +1,25 @@
+import _ from "lodash"
+import { Request, Response } from "express"
+import AwsS3 from "../../classes/aws-s3"
+import { createS3KeyGenerateUUID } from "../../utils/s3/create-s3-key"
+import addProfilePictureRecord from "../../utils/db-operations/write/simultaneous-writes/add-profile-picture-and-update-user"
+
+export default async function uploadProfilePicture (req: Request, res: Response): Promise<Response> {
+	try {
+		const user = req.user
+		if (_.isUndefined(req.file)) return res.status(400).json({ message: "No image uploaded" })
+
+		const fileBuffer = req.file.buffer
+		const fileName = req.file.originalname
+
+		const uploadImageToS3Key = createS3KeyGenerateUUID("profile-pictures", fileName)
+		const profilePictureUrl = await AwsS3.getInstance().uploadImage(fileBuffer, uploadImageToS3Key.key)
+
+		await addProfilePictureRecord(profilePictureUrl, fileName, uploadImageToS3Key.uuid, user.user_id)
+
+		return res.status(200).json({ profilePictureUrl })
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ error: "Internal Server Error: Unable to Upload Profile Picture" })
+	}
+}
