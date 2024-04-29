e24d66a4ea2749fd33487d95455cafa37f784393
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-29 10:41:45 -0400

    Retrieve wallet public key (#29)
    * Retrieve wallet public key
    
    * Update mint-spl-helper.ts

--- src/routes/personal-info-routes.ts
+++ src/routes/personal-info-routes.ts
@@ -3,7 +3,9 @@ import express from "express"
 import setDefaultCurrency from "../controllers/personal-info/set-default-currency"
 import setDefaultSiteTheme from "../controllers/personal-info/set-default-site-theme"
 import retrievePersonalInfo from "../controllers/personal-info/retrieve-personal-info"
+import retrieveWalletPublicKey from "../controllers/personal-info/retrieve-wallet-public-key"
 
+import attachSolanaWalletByUserId from "../middleware/attach/attach-solana-wallet-by-user-id"
 import validateSetDefaultCurrency from "../middleware/request-validation/personal-info/validate-set-default-currency"
 import validateSetDefaultSiteTheme from "../middleware/request-validation/personal-info/validate-set-default-site-theme"
 
@@ -12,5 +14,6 @@ const personalInfoRoutes = express.Router()
 personalInfoRoutes.get("/retrieve-personal-info", retrievePersonalInfo)
 personalInfoRoutes.post("/set-default-currency/:defaultCurrency", validateSetDefaultCurrency, setDefaultCurrency)
 personalInfoRoutes.post("/set-default-site-theme/:defaultSiteTheme", validateSetDefaultSiteTheme, setDefaultSiteTheme)
+personalInfoRoutes.get("/retrieve-wallet-public-key", attachSolanaWalletByUserId, retrieveWalletPublicKey)
 
 export default personalInfoRoutes
