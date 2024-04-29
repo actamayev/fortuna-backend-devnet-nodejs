952db5f07066dc9ad6e59e1bd95133d9e5d2aab7
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-28 22:45:00 -0400

    Creator public page (#27)
    * MIGRATION: Make username unique
    
    * Fetch creator data
    
    * Confirm user is approved to be a creator
    
    * Cleanup/updating endpoitns
    
    * Cleanup

---
+++ src/controllers/videos/get-videos-by-creator-username.ts
@@ -0,0 +1,24 @@
+import _ from "lodash"
+import { Response, Request } from "express"
+import transformVideosByCreatorUsername from "../../utils/transform/transform-videos-by-creator-username"
+import retrieveVideosByCreatorUsername from "../../utils/db-operations/read/credentials/retrieve-videos-by-creator-username"
+
+export default async function getVideosByCreatorUsername (req: Request, res: Response): Promise<Response> {
+	try {
+		const creatorUsername = req.params.creatorUsername as string
+
+		const retrievedVideoData = await retrieveVideosByCreatorUsername(creatorUsername)
+		if (_.isNull(retrievedVideoData)) return res.status(400).json({ message: "Unable to find creator associated with this username" })
+
+		const transformedVideoData = await transformVideosByCreatorUsername(retrievedVideoData)
+		if (_.isNull(transformedVideoData)) return res.status(400).json({ message: "Unable to find creator associated with this username"})
+
+		return res.status(200).json({
+			videoData: transformedVideoData.videoData,
+			creatorData: transformedVideoData.creatorData
+		})
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ error: "Internal Server Error: Unable to get videos by creator username" })
+	}
+}
