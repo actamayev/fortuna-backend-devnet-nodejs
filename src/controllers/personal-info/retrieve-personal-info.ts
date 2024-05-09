6ac5bdb2e23665a267cbaf328dbb7296c20f76f8
Ariel Tamayev <actamayev@gmail.com>
2024-05-08 20:14:50 -0400

    Retrieve email (#46)
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/controllers/personal-info/retrieve-personal-info.ts
+++ src/controllers/personal-info/retrieve-personal-info.ts
@@ -1,4 +1,6 @@
+import _ from "lodash"
 import { Request, Response } from "express"
+import Encryptor from "../../classes/encryptor"
 import retrieveProfilePictureUrlByUserId from "../../db-operations/read/profile-picture/retrieve-profile-picture-url-by-user-id"
 
 export default async function retrievePersonalInfo(req: Request, res: Response): Promise<Response> {
@@ -6,8 +8,14 @@ export default async function retrievePersonalInfo(req: Request, res: Response):
 		const { user } = req
 		const profilePictureUrl = await retrieveProfilePictureUrlByUserId(user.user_id)
 
+		let email
+		if (!_.isNull(user.email__encrypted)) {
+			const encryptor = new Encryptor()
+			email = await encryptor.deterministicDecrypt(user.email__encrypted, "EMAIL_ENCRYPTION_KEY")
+		}
 		return res.status(200).json({
 			username: user.username,
+			email,
 			defaultCurrency: user.default_currency,
 			defaultSiteTheme: user.default_site_theme,
 			isApprovedToBeCreator: user.is_approved_to_be_creator,
