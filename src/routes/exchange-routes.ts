d540834be3b9595704cf2a56291b2aa43c443d78
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-16 16:18:37 -0400

    Update exchange-routes.ts

--- src/routes/exchange-routes.ts
+++ src/routes/exchange-routes.ts
@@ -59,6 +59,8 @@ exchangeRoutes.post(
 	placeSecondaryMarketSplBid
 )
 
+// eslint-disable-next-line max-len
+// TODO: Add a middleware that checks the the amount that the user is asking for is less the number of shares the user currently has minus the sum of the number of shares in open asks
 exchangeRoutes.post(
 	"/create-spl-ask",
 	validateCreateSplAsk,
