eea7ba8fe84448dff2f509c94c2e0d72958bf755
Ariel Tamayev <actamayev@gmail.com>
2024-05-27 10:42:15 -0400

    Delete secondary market (#60)
    * Delete secondary market
    
    * Cleanup
    
    * Delete check-if-spl-exists-by-id.ts
    
    ---------
    
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/middleware/attach/attach-spl-details-by-public-key.ts
+++ src/middleware/attach/attach-spl-details-by-public-key.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express"
 import retrieveSplDetailsByPublicKey from "../../db-operations/read/spl/retrieve-spl-details-by-public-key"
 import transformSplDetailsRetrievedByPublicKey from "../../utils/transform/transform-spl-details-retrieved-by-public-key"
 
-export async function attachSplDetailsByPublicKeyForPrimarySplPurchase(
+export default async function attachSplDetailsByPublicKeyForPrimarySplPurchase(
 	req: Request,
 	res: Response,
 	next: NextFunction
@@ -19,43 +19,5 @@ export async function attachSplDetailsByPublicKeyForPrimarySplPurchase(
 	} catch (error) {
 		console.error(error)
 		return res.status(500).json({ error: "Internal Server Error: Unable to Attach Spl Details by Public Key (Primary)" })
-	}
-}
-
-export async function attachSplDetailsByPublicKeyForSecondarySplBid(
-	req: Request,
-	res: Response,
-	next: NextFunction
-): Promise<Response | void> {
-	try {
-		const createSplBid = req.body.createSplBid as CreateSplBidData
-		const splDetails = await retrieveSplDetailsByPublicKey(createSplBid.splPublicKey)
-
-		if (_.isNull(splDetails)) return res.status(400).json({ message: "Unable to find Token Details" })
-		const transformedSplDetails = transformSplDetailsRetrievedByPublicKey(splDetails)
-		req.splDetails = transformedSplDetails
-		next()
-	} catch (error) {
-		console.error(error)
-		return res.status(500).json({ error: "Internal Server Error: Unable to Attach Spl Details by Public Key (Bid)" })
-	}
-}
-
-export async function attachSplDetailsByPublicKeyForSecondarySplAsk(
-	req: Request,
-	res: Response,
-	next: NextFunction
-): Promise<Response | void> {
-	try {
-		const createSplAsk = req.body.createSplAsk as CreateSplAskData
-		const splDetails = await retrieveSplDetailsByPublicKey(createSplAsk.splPublicKey)
-
-		if (_.isNull(splDetails)) return res.status(400).json({ message: "Unable to find Token Details" })
-		const transformedSplDetails = transformSplDetailsRetrievedByPublicKey(splDetails)
-		req.splDetails = transformedSplDetails
-		next()
-	} catch (error) {
-		console.error(error)
-		return res.status(500).json({ error: "Internal Server Error: Unable to Attach Spl Details by Public Key (Ask)" })
 	}
 }
--- src/routes/exchange-routes.ts
+++ src/routes/exchange-routes.ts
@@ -1,40 +1,17 @@
 import express from "express"
 
-import placeSplAsk from "../controllers/exchange/ask/place-spl-ask"
-import placeSplBid from "../controllers/exchange/bid/place-spl-bid"
-import cancelSplAsk from "../controllers/exchange/ask/cancel-spl-ask"
-import cancelSplBid from "../controllers/exchange/bid/cancel-spl-bid"
-import retrieveUserOrders from "../controllers/exchange/retrieve-user-orders"
 import primarySplTokenPurchase from "../controllers/exchange/primary-spl-token-purchase"
-import retrieveOpenOrdersBySplId from "../controllers/exchange/retrieve-open-orders-by-spl-id"
 import purchaseExclusiveContentAccess from "../controllers/exchange/purchase-exclusive-content-access"
 
-import {
-	attachSplDetailsByPublicKeyForSecondarySplBid,
-	attachSplDetailsByPublicKeyForSecondarySplAsk,
-	attachSplDetailsByPublicKeyForPrimarySplPurchase
-} from "../middleware/attach/attach-spl-details-by-public-key"
 import attachExclusiveVideoData from "../middleware/attach/attach-exclusive-video-data"
-import confirmBidCreator from "../middleware/confirmations/exchange/bid/confirm-bid-creator"
-import confirmAskCreator from "../middleware/confirmations/exchange/ask/confirm-ask-creator"
-import {
-	confirmUserHasEnoughSolToBidForSecondaryTokens,
-	confirmUserHasEnoughSolToPurchasePrimaryTokens
-} from "../middleware/confirmations/exchange/confirm-user-has-enough-sol-to-purchase-tokens"
 import attachSolanaWalletByUserId from "../middleware/attach/attach-solana-wallet-by-user-id"
-import confirmSplExistsById from "../middleware/confirmations/exchange/confirm-spl-exists-by-id"
-import validateCancelSplBid from "../middleware/request-validation/exchange/bid/validate-cancel-bid"
-import validateCancelSplAsk from "../middleware/request-validation/exchange/ask/validate-cancel-ask"
-import confirmUserHasEnoughTokensToCreateSplAsk
-	from "../middleware/confirmations/exchange/ask/confirm-user-has-enough-tokens-to-create-spl-ask"
-import validateSplIdInParams from "../middleware/request-validation/exchange/validate-spl-id-in-params"
-import validateCreateSplBid from "../middleware/request-validation/exchange/bid/validate-create-spl-bid"
-import validateCreateSplAsk from "../middleware/request-validation/exchange/ask/validate-create-spl-ask"
+import confirmUserHasEnoughSolToPurchasePrimaryTokens
+	from "../middleware/confirmations/exchange/confirm-user-has-enough-sol-to-purchase-tokens"
 import confirmEnoughSharesInEscrowToCompletePurchase
 	from "../middleware/confirmations/exchange/primary/confirm-enough-shares-in-escrow-to-complete-purchase"
 import validateVideoUUIDInParams from "../middleware/request-validation/videos/validate-video-uuid-in-params"
 import validatePurchaseSplTokens from "../middleware/request-validation/exchange/validate-purchase-spl-tokens"
-import confirmPrimarySplSharesSoldOut from "../middleware/confirmations/exchange/confirm-primary-spl-shares-sold-out"
+import attachSplDetailsByPublicKeyForPrimarySplPurchase from "../middleware/attach/attach-spl-details-by-public-key"
 import confirmUserDoesntAlreadyHaveExclusiveAccess
 	from "../middleware/confirmations/exchange/instant-exclusive-access/confirm-user-doesnt-already-have-exclusive-access"
 import confirmUserHasEnoughSolToPurchaseExclusiveAccess
@@ -58,49 +35,6 @@ exchangeRoutes.post(
 )
 
 exchangeRoutes.post(
-	"/create-spl-bid",
-	validateCreateSplBid,
-	attachSplDetailsByPublicKeyForSecondarySplBid,
-	confirmPrimarySplSharesSoldOut,
-	attachSolanaWalletByUserId,
-	confirmUserHasEnoughSolToBidForSecondaryTokens,
-	placeSplBid
-)
-
-exchangeRoutes.post(
-	"/create-spl-ask",
-	validateCreateSplAsk,
-	attachSplDetailsByPublicKeyForSecondarySplAsk,
-	confirmPrimarySplSharesSoldOut,
-	attachSolanaWalletByUserId,
-	confirmUserHasEnoughTokensToCreateSplAsk,
-	placeSplAsk
-)
-
-exchangeRoutes.post(
-	"/cancel-spl-bid/:splBidId",
-	validateCancelSplBid,
-	confirmBidCreator,
-	cancelSplBid
-)
-
-exchangeRoutes.post(
-	"/cancel-spl-ask/:splAskId",
-	validateCancelSplAsk,
-	confirmAskCreator,
-	cancelSplAsk
-)
-
-exchangeRoutes.get("/retrieve-my-orders", attachSolanaWalletByUserId, retrieveUserOrders)
-
-exchangeRoutes.get(
-	"/retrieve-open-orders-by-spl-id/:splId",
-	validateSplIdInParams,
-	confirmSplExistsById,
-	retrieveOpenOrdersBySplId
-)
-
-exchangeRoutes.post(
 	"/purchase-exclusive-content-access/:videoUUID",
 	validateVideoUUIDInParams,
 	attachExclusiveVideoData,
@@ -110,5 +44,4 @@ exchangeRoutes.post(
 	purchaseExclusiveContentAccess
 )
 
-// FUTURE TODO: Add routes to edit an order.
 export default exchangeRoutes
