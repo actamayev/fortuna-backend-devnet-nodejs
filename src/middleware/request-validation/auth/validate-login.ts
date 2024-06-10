30194230317e159c8d52dbd94b15c2945100dbbc
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-10 15:52:49 -0400

    Username should be at least 3 characters

--- src/middleware/request-validation/auth/validate-login.ts
+++ src/middleware/request-validation/auth/validate-login.ts
@@ -5,7 +5,7 @@ import passwordValidatorSchema from "../../joi/password-validator"
 
 const loginInformationSchema = Joi.object({
 	loginInformation: Joi.object({
-		contact: Joi.string().required(),
+		contact: Joi.string().required().min(3),
 		password: passwordValidatorSchema.required(),
 	}).required()
 }).required()
