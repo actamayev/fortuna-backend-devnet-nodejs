f633322bc1209d0ab3f342d7b724368c3f57d5d3
mintfortuna <167586092+mintfortuna@users.noreply.github.com>
2024-06-11 15:50:25 -0400

    Hide youtube endpoints from client (#71)
    * Hide youtube endpoitns
    
    * Update google-auth-routes.ts
    
    ---------
    
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/routes/auth/google-auth-routes.ts
+++ src/routes/auth/google-auth-routes.ts
@@ -1,21 +1,21 @@
 import express from "express"
 
-import youtubeAuthCallback from "../../controllers/auth/youtube-auth-callback"
+// import youtubeAuthCallback from "../../controllers/auth/youtube-auth-callback"
 import googleLoginAuthCallback from "../../controllers/auth/google-login-auth-callback"
 
-import jwtVerifyAttachUser from "../../middleware/jwt/jwt-verify-attach-user"
-import validateYouTubeAuthCallback from "../../middleware/request-validation/auth/google/validate-youtube-auth-callback"
+// import jwtVerifyAttachUser from "../../middleware/jwt/jwt-verify-attach-user"
+// import validateYouTubeAuthCallback from "../../middleware/request-validation/auth/google/validate-youtube-auth-callback"
 import validateGoogleLoginAuthCallback from "../../middleware/request-validation/auth/google/validate-google-login-auth-callback"
 
 const googleAuthRoutes = express.Router()
 
 googleAuthRoutes.post("/login-callback", validateGoogleLoginAuthCallback, googleLoginAuthCallback)
 
-googleAuthRoutes.post(
-	"/youtube-callback",
-	validateYouTubeAuthCallback,
-	jwtVerifyAttachUser,
-	youtubeAuthCallback
-)
+// googleAuthRoutes.post(
+// 	"/youtube-callback",
+// 	validateYouTubeAuthCallback,
+// 	jwtVerifyAttachUser,
+// 	youtubeAuthCallback
+// )
 
 export default googleAuthRoutes
