17f31d24111275bec11850324d8609c5ff5490a9
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-11 15:46:45 -0400

    Hide youtube endpoitns

--- src/routes/youtube-routes.ts
+++ src/routes/youtube-routes.ts
@@ -2,10 +2,18 @@ import express from "express"
 
 import retrieveUserYouTubeInfo from "../controllers/youtube/retrieve-user-youtube-info"
 
+import jwtVerifyAttachUser from "../middleware/jwt/jwt-verify-attach-user"
 import attachYouTubeAccessToken from "../middleware/attach/attach-youtube-access-token"
 
+// Youtube endpoints are currently not accessible to client.
+// Work on perfecting functionality, fix the bug that revokes the user's Youtube access token
 const youtubeRoutes = express.Router()
 
-youtubeRoutes.get("/retrieve-user-youtube-info", attachYouTubeAccessToken, retrieveUserYouTubeInfo)
+youtubeRoutes.get(
+	"/retrieve-user-youtube-info",
+	jwtVerifyAttachUser,
+	attachYouTubeAccessToken,
+	retrieveUserYouTubeInfo
+)
 
 export default youtubeRoutes
