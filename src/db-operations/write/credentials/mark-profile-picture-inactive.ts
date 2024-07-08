16e7e67d00f15e966a56a0e235ffb10d8b6498e8
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-08 17:55:17 -0400

    Update mark-profile-picture-inactive.ts

--- src/db-operations/write/credentials/mark-profile-picture-inactive.ts
+++ src/db-operations/write/credentials/mark-profile-picture-inactive.ts
@@ -1,6 +1,5 @@
 import PrismaClientClass from "../../../classes/prisma-client"
 
-// TODO: Only retrieve active pfps and banners
 export default async function markProfilePictureLinkInactive(userId: number): Promise<void> {
 	try {
 		const prismaClient = await PrismaClientClass.getPrismaClient()
