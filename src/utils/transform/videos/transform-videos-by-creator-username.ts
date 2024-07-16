67eae26bc2b6b6c7029de5b3975c67bc5387bddc
Ariel Tamayev <actamayev@gmail.com>
2024-07-16 15:24:47 -0400

    Retrieve number of videos by creator (#99)
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/utils/transform/videos/transform-videos-by-creator-username.ts
+++ src/utils/transform/videos/transform-videos-by-creator-username.ts
@@ -59,7 +59,8 @@ export default async function transformVideosByCreatorUsername(
 				socialLink: singleData.social_link
 			})) || [],
 			creatorProfilePictureUrl: retrievedVideoData.profile_picture_image_url,
-			channelBannerPictureUrl: retrievedVideoData.channel_banner_image_url
+			channelBannerPictureUrl: retrievedVideoData.channel_banner_image_url,
+			numberOfVideos: retrievedVideoData.videos.length
 		}
 
 		return { videoData, creatorData }
