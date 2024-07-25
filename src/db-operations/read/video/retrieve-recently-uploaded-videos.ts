d124a16ba8d6edfcc12fde92bfb65dc6ae1bf098
Ariel Tamayev <actamayev@gmail.com>
2024-07-25 19:18:38 -0400

    Show all recently uploaded videos (#111)
    * Get recently uploaded videos
    
    * Update retrieve-recently-uploaded-videos.ts
    
    * Update retrieve-recently-uploaded-videos.ts
    
    ---------
    
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

---
+++ src/db-operations/read/video/retrieve-recently-uploaded-videos.ts
@@ -0,0 +1,103 @@
+import PrismaClientClass from "../../../classes/prisma-client"
+
+// eslint-disable-next-line max-lines-per-function
+export default async function retrieveRecentlyUploadedVideos(): Promise<RetrievedHomePageVideosFromDB[]> {
+	try {
+		const prismaClient = await PrismaClientClass.getPrismaClient()
+		const mediaDetails = await prismaClient.video.findMany({
+			where: {
+				video_listing_status: {
+					not: "UNLISTED"
+				},
+				video_creator: {
+					is_active: true
+				}
+			},
+			select: {
+				video_id: true,
+				video_name: true,
+				video_listing_status: true,
+				description: true,
+				is_video_featured: true,
+				creator_user_id: true,
+				is_video_exclusive: true,
+				uuid: true,
+				created_at: true,
+				uploaded_image: {
+					select: {
+						image_url: true
+					}
+				},
+				uploaded_video: {
+					select: {
+						video_duration_seconds: true
+					}
+				},
+				video_access_tier: {
+					select: {
+						tier_number: true,
+						purchases_allowed_for_this_tier: true,
+						tier_access_price_usd: true,
+						is_sold_out: true
+					}
+				},
+				video_creator: {
+					select: {
+						username: true,
+						profile_picture: {
+							select: {
+								image_url: true
+							},
+							where: {
+								is_active: true
+							}
+						},
+						channel_banner: {
+							select: {
+								image_url: true
+							},
+							where: {
+								is_active: true
+							}
+						},
+						channel_name: {
+							select: {
+								channel_name: true
+							}
+						}
+					}
+				},
+				video_like_status: {
+					select: {
+						user_id: true
+					},
+					where: {
+						is_active: true
+					}
+				},
+				_count: {
+					select: {
+						exclusive_video_access_purchase: true
+					}
+				}
+			},
+			orderBy: {
+				created_at: "desc"
+			}
+		})
+
+		const filteredVideos = mediaDetails
+			.filter(video => video.video_creator.username !== null)
+			.map(video => ({
+				...video,
+				numberOfExclusivePurchasesSoFar: video.is_video_exclusive ? video._count.exclusive_video_access_purchase : null
+			}))
+			// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars
+			.map(({ _count, ...rest }) => rest) // Remove _count property
+
+		return filteredVideos as RetrievedHomePageVideosFromDB[]
+	} catch (error) {
+		console.error(error)
+		throw error
+	}
+}
