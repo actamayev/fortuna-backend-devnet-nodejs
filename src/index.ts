5afddc0f794ab1df437cb2ed6de08b489876f33a
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-06 16:14:40 -0400

    Remove the allowed origins temporarily

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,10 @@ app.use(cors({
 	origin: function (origin, callback) {
 		// Allow requests with no origin (like mobile apps, curl requests, or Postman)
 		if (!origin) return callback(null, true)
-		if (allowedOrigins.indexOf(origin) === -1) {
-			const msg = "The CORS policy for this site does not allow access from the specified Origin."
-			return callback(new Error(msg), false)
-		}
+		// if (allowedOrigins.indexOf(origin) === -1) {
+		// 	const msg = "The CORS policy for this site does not allow access from the specified Origin."
+		// 	return callback(new Error(msg), false)
+		// }
 		return callback(null, true)
 	},
 	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
