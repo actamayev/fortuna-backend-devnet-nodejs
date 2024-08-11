b729408d9618cead61583b3de43343533d87622e
Ariel Tamayev <actamayev@gmail.com>
2024-08-10 20:06:19 -0400

    New mainnet cluster URL (#118)
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/utils/solana/get-cluster-url-by-env.ts
+++ src/utils/solana/get-cluster-url-by-env.ts
@@ -4,7 +4,7 @@ export function getClusterUrlByEnv(): string {
 	const env = process.env.NODE_ENV
 
 	if (env === "production-mainnet") {
-		return clusterApiUrl("mainnet-beta")
+		return "https://sleek-sparkling-waterfall.solana-mainnet.quiknode.pro/2304791a7554e508e4d429ba1adb4f91d5a9c7c5"
 	}
 
 	return clusterApiUrl("devnet")
