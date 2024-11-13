c4064b328c2d8c8baba522546582f7b49253c359
actamayev <75034340+actamayev@users.noreply.github.com>
2024-11-13 18:56:28 -0500

    Fix origins (#125)

--- src/utils/get-allowed-origins.ts
+++ src/utils/get-allowed-origins.ts
@@ -1,8 +1,8 @@
 export default function allowedOrigins(): string[] {
-	if (process.env.NODE_ENV === "production-devnet") {
+	if (process.env.NODE_ENV === "production") {
+		return [ "https://createfortuna.com", "https://www.createfortuna.com" ]
+	} else if (process.env.NODE_ENV === "staging") {
 		return [ "https://devnet.createfortuna.com" ]
-	} else if (process.env.NODE_ENV === "production-mainnet") {
-		return [ "https://www.createfortuna.com" ]
 	} else {
 		return [ "http://localhost:3000" ]
 	}
